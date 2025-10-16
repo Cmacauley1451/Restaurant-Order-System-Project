ac9b50c5cf64f658e48a34cc3d97cb220e23d2c5
Connor Macauley <cmacauley1451@gmail.com>
2025-10-16 01:18:13 +0100

    Add files via upload

--- Restaurant_Project/src/Kitchen.java
+++ Restaurant_Project/src/Kitchen.java
@@ -1,2 +1,12 @@
 public class Kitchen {
+
+    private String food;
+    private int amount;
+    private boolean isMeal;
+
+    public Kitchen(String food, int amount, boolean meal){
+        this.food = food;
+        this.amount = amount;
+        this.isMeal = meal;
+    }
 }
