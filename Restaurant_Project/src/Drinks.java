d501742e77fc8a9366e063b06487c62dfdbe7912
Connor Macauley <cmacauley1451@gmail.com>
2025-10-16 01:07:39 +0100

    Add files via upload

--- Restaurant_Project/src/Drinks.java
+++ Restaurant_Project/src/Drinks.java
@@ -1,7 +1,7 @@
 public class Drinks {
 
     private String drink;
-    private boolean ice;
+    private boolean ice; /* Allows user to decide whether they want ice or no ice */
 
     public Drinks(String drink, boolean ice){
         this.drink = drink;
---
+++ Restaurant_Project/src/Restaurant.java
@@ -0,0 +1,101 @@
+import java.util.*;
+import java.io.*;
+
+
+public class Restaurant {
+    Scanner scan = new Scanner(System.in);
+    int choice;
+
+    public static void main(String[] args) {
+        Restaurant Condogz = new Restaurant();
+        Condogz.menu();
+    }
+
+    private void menu() {
+        try {
+
+            System.out.println("Welcome to Condogz, Press enter to order");
+            scan.nextLine();
+
+            System.out.println("             Menu            ");
+            System.out.println("______________________________");
+            System.out.println("1.Burgers          2.Chicken");
+            System.out.println("3.Drinks           4.Desserts");
+
+            System.out.println(" ");
+            System.out.println("What would you like to view?");
+
+        //          Menu options
+
+        if (scan.hasNextInt()) {
+            choice = scan.nextInt();
+            scan.nextLine();
+            if (choice >= 1 & choice <= 4) { // Double verification of valid input
+
+                if (choice == 1) {System.out.println("Example1");}
+                else if (choice == 2) {System.out.println("example2");}
+                else if (choice == 3) {chooseDrink();}
+            }
+            else {System.out.println("Error: Please input a number between 1 and 4");
+                System.out.println("returning to menu in 3 seconds...");
+                Thread.sleep(3000);
+                menu();}
+        }
+
+        else {System.out.println("Error: Invalid Input, Please input a number");
+            System.out.println("returning to menu in 3 seconds...");
+            Thread.sleep(3000);
+            menu();}
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /*                      DRINKS                     */
+
+    private void chooseDrink() {
+        try {
+            int drinkChoice;
+            System.out.println("            Drinks          ");
+            System.out.println("_____________________________");
+            System.out.println("1.Coke          2.Diet Coke\n3.Coke Zero\t\t4.Sprite\n5.Fanta\t\t\t6.Water\n7.Back");
+
+            if (scan.hasNextInt()) {
+                drinkChoice = scan.nextInt();
+                // Checks if drinkChoice is within a valid range (1-7)
+                if (drinkChoice >= 1 && drinkChoice <= 7) {
+                    //                  Drink Choices
+                    if (drinkChoice == 1) {System.out.println("Example1");}
+                    else if (drinkChoice == 2) {System.out.println("Example2");}
+                    else if (drinkChoice == 3) {System.out.println("Example3");}
+                    else if (drinkChoice == 4) {System.out.println("Example4");}
+                    else if (drinkChoice == 5) {System.out.println("Example5");}
+                    else if (drinkChoice == 6) {System.out.println("Example6");}
+                    else if (drinkChoice == 7) {System.out.println("Example7");}
+                    else System.out.println("Error: invalid input");
+
+                } else {
+                    System.out.println("Error: Please enter a number between 1 and 7.");
+                    System.out.println("Returning to menu in 3 seconds...");
+                    Thread.sleep(3000);
+                    chooseDrink();
+                }
+            } else {
+                System.out.println("Error: Invalid input. Please enter a number!");
+                scan.next(); // avoids infinite loop
+                System.out.println("Returning to menu in 3 seconds...");
+                Thread.sleep(3000);
+                chooseDrink();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
+
+
+
+
+
